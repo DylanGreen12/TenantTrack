2019c66742e455bac7f742cd69c3a7019403c651
DylanGreen12 <98060477+DylanGreen12@users.noreply.github.com>
2025-09-03 16:44:51 -0500

    add properties table

---
+++ Selu383.SP25.P03.Api/Features/Properties/Property.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Selu383.SP25.P03.Api.Features.Users;
+
+namespace Selu383.SP25.P03.Api.Features.Properties
+{
+    public class Property
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(120)]
+        public string Name { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        [StringLength(2)]
+        public string State { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string ZipCode { get; set; }
+
+        [Required]
+        public int UserId { get; set; } 
+        
+        [ForeignKey("UserId")]
+        public virtual User User { get; set; } 
+    }
+}
---
+++ Selu383.SP25.P03.Api/Migrations/20250903214315_PropteryTable.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Selu383.SP25.P03.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class PropteryTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(120)", maxLength: 120, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    State = table.Column<string>(type: "nvarchar(2)", maxLength: 2, nullable: false),
+                    ZipCode = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Properties_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Properties_UserId",
+                table: "Properties",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Properties");
+        }
+    }
+}
