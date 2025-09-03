41fe3e6614b7921c132283b38d6c2350df5b1ee3
DylanGreen12 <98060477+DylanGreen12@users.noreply.github.com>
2025-09-03 14:31:46 -0500

    fix azure db

--- TenantTrack.Api/Features/Users/UserDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TenantTrack.Api.Features.Users
-{
-    public class UserDto
-    {
-        public int Id { get; set; }
-        public string? UserName { get; set; }
-        public string[]? Roles { get; set; }
-    }
-}
