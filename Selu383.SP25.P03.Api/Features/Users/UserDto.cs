8092d1fb5adeee88f00ecdfed900645d416ada51
DylanGreen12 <98060477+DylanGreen12@users.noreply.github.com>
2025-10-02 03:04:25 -0500

    add contact info
    Additions:
    Email and phone on users, ability to edit contact info, property view now displays owner based on userId
    
    Issues:
    phone and email are null on signup and the method to grab basic user info has no authen because it gave errors

--- Selu383.SP25.P03.Api/Features/Users/UserDto.cs
+++ Selu383.SP25.P03.Api/Features/Users/UserDto.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public string? UserName { get; set; }
         public string[]? Roles { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
     }
 }
